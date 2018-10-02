da70eee715a2921bb13de637d339a986f61a266c
ookiiani <jakestr1999@gmail.com>
2018-10-02 19:29:21 +0100

    Annotations to the basic version of the program.

--- OOPCalculator/Project1/Source.cpp
+++ OOPCalculator/Project1/Source.cpp
@@ -9,11 +9,13 @@ const int maxNumOfCalculations = 6;
 
 int main() 			// multiply two numbers
 {
-	void doACalculation(int calculationArray[][maxNumOfCalculations], int);
+	void doACalculation(int calculationArray[][maxNumOfCalculations], int); // Forward declaration //
 	bool wantToContinue();
-	int calculations = 0;
 
-	int calculationArray[3][maxNumOfCalculations];
+	int calculations = 0;
+	int calculationArray[3][maxNumOfCalculations]; // This will hold a maximum of three calculations //
+	// It is a 2D array, so for each array entry there is a corresponding array (of size maxNumOfCalculations) //
+	// This array will hold data in the order number1, number2, and then product //
 
 	do
 	{
@@ -23,20 +25,19 @@ int main() 			// multiply two numbers
 	
 	cout << "What filename should the calculations be written to? (i.e. 'calculations'): ";
 	string filename;
-	cin >> filename;
-
+	cin >> filename; // Allow the user to input a chosen filename //
 	ofstream calculationsFile;
-	calculationsFile.open(filename + ".txt");
+	calculationsFile.open(filename + ".txt"); // Open a new file for writing //
 
 	cout << "Calculations:" << endl;
-	calculationsFile << "Calculations:\n";
+	calculationsFile << "Calculations:\n"; // Write the string to the file //
 	for (int x = 0; x < calculations; x++)
 	{
 		cout << calculationArray[x][0] << " * " << calculationArray[x][1] << " = " << calculationArray[x][2] << endl;
-		calculationsFile << calculationArray[x][0] << " * " << calculationArray[x][1] << " = " << calculationArray[x][2] << endl;
+		calculationsFile << calculationArray[x][0] << " * " << calculationArray[x][1] << " = " << calculationArray[x][2] << endl; // Write score string to file //
 	}
 
-	calculationsFile.close();
+	calculationsFile.close(); // Close the file //
 
 	cout << "Program ended. Press any key to exit!" << endl;
 	system("pause");	//to hold the output screen
@@ -52,7 +53,7 @@ void doACalculation(int calculationArray[][maxNumOfCalculations], int calculatio
 	cout << "Please enter number 2: ";
 	cin >> number2;
 	multiply(number1, number2, &product);
-	calculationArray[calculations][0] = number1;
+	calculationArray[calculations][0] = number1; // Insert the data into the nested array at the predetermined positions (see declaration above for more information) //
 	calculationArray[calculations][1] = number2;
 	calculationArray[calculations][2] = product;
 	cout << "The product is: " << product << "\n\n";
